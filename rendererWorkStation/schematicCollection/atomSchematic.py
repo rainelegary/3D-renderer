4bc7479f014e0e3d80d1ec3348aaa6a53ff4a978
Rainethhh <Rainethcubed@gmail.com>
2021-09-02 12:14:27 -0500

    atom schematic cleaned up

--- rendererWorkStation/schematicCollection/atomSchematic.py
+++ rendererWorkStation/schematicCollection/atomSchematic.py
@@ -6,114 +6,11 @@ from linearAlgebra.specialMatrices import *
 from varStorage import *
 
 
-def initializeAtom(nOrbitals, nElectrons, nOrbitalSteps, electronSize=5):
-	phi = (math.sqrt(5)+1)/2
-	ellipticalRange = [1, phi*phi]
-	radiusRange = [1/phi, phi]
-	speedRange = [1/phi, phi]
-
-	allOrbitals = []
-
-	for orbital in range(nOrbitals):
-		
-		orbitProperties = {'points': [], 'electron locations': [], 'color': '#036BFC', 'point size': electronSize}
-
-		# angles and dynamic radius
-		angleRadList = dynamicRadius(radiusRange, ellipticalRange, speedRange, nOrbitalSteps)
-		orbitProperties['angle & radius list'] = angleRadList
-		orbitProperties['rotation speeds'] = [random.uniform(-0.2, 0.2) for i in range(3)]
-
-		# transformation into 3D space
-		orbitProperties = transformOrbital(orbitProperties)
-
-		allOrbitals.append(orbitProperties)
-	
-	orbitalSet = distributeElectrons(allOrbitals, nElectrons)
-
-	return orbitalSet
-
-
-def distributeElectrons(orbitalSet, nElectrons):
-	nOrbitals = len(orbitalSet)
-
-	for electronN in range(nElectrons):
-		orbitIndex = random.randint(0, nOrbitals - 1)
-		nSteps = len(orbitalSet[orbitIndex]['points'])
-		startingAngle = random.randint(0, nSteps - 1)
-
-		orbitalSet[orbitIndex]['electron locations'].append(startingAngle)
-	
-	return orbitalSet
-
-
-def dynamicRadius(radiusRange, ellipticalRange, speedRange, nOrbitalSteps):
-	axisA = random.uniform(radiusRange[0], radiusRange[1])
-	axisB = axisA * random.uniform(ellipticalRange[0], ellipticalRange[1])
-	orbitalSpeed = random.uniform(speedRange[0], speedRange[1])
-	nSteps = round(nOrbitalSteps/orbitalSpeed)
-	angleOffset = random.uniform(0, 2*math.pi)
-	angleList = [(stepN*2*math.pi)/nSteps for stepN in range(nSteps)]
-	radiusList = [calcRadius(axisA, axisB, angleList[i] + angleOffset) for i in range(nSteps)]
-
-	angleRadList = [{'angle': angleList[i], 'radius': radiusList[i]} for i in range(nSteps)]
-
-	return angleRadList
-
-
-def transformOrbital(orbitProperties):
-	angleRadList = orbitProperties['angle & radius list']
-	nSteps = len(angleRadList)
-
-	thetas = [random.uniform(0, 2*math.pi) for i in range(3)]
-	multMatrix = matricesData.regRotationMat(thetas)
-	
-	cartesianList = polarToCartesian(angleRadList)
-	pointsMatrix = findTranspose(cartesianList)
-	projectedPoints = np.dot(multMatrix, pointsMatrix)
-	points = findTranspose(projectedPoints)
-	
-	orbitProperties['points'] = points
-
-	return orbitProperties
-
-		
-def randomVector(dimensions):
-    components = [np.random.normal() for i in range(dimensions)]
-    length = math.sqrt(sum(x*x for x in components))
-    vector = [x/length for x in components]
-    return vector
-
-
-def convertToUnitVec(vector):
-	length = math.sqrt(sum(x*x for x in vector))
-	for i in range(len(vector)):
-		vector[i] /= length
-	
-	return vector
-
-
-def calcRadius(a, b, t):
-	return a*b/math.sqrt(pow(a*math.sin(t), 2)+pow(b*math.cos(t), 2))
-
-
-def passElectrons(electrons, nSteps):
-	for e in range(len(electrons)):
-		if electrons[e] in [nSteps - 1, nSteps]:
-			electrons[e] = 0
-		else:
-			electrons[e] += 1
-	return electrons
-
-
 class AtomSchematic(Schematic):
 	def __init__(self, nOrbitals, nElectrons, nOrbitalSteps, electronSize=5):
-		self.orbitalSet = initializeAtom(nOrbitals, nElectrons, nOrbitalSteps, electronSize=electronSize)
+		self.initializeOrbitals(nOrbitals, nElectrons, nOrbitalSteps, electronSize=electronSize)
 		self.currentFrame = self.orbitalSet
 		self.updateSchematic()
-
-
-	# def __iter__(self):
-	# 	return iter(self.schematic)
 
 
 	def updateSchematic(self):
@@ -123,7 +20,7 @@ class AtomSchematic(Schematic):
 		# changing colors, not sure in what way yet
 		self.createSchemSets()
 
-		
+	
 	def createSchemSets(self):
 		schematic = []
 		orbitals = self.orbitalSet
@@ -141,6 +38,81 @@ class AtomSchematic(Schematic):
 		self.schematic = schematic
 
 
+	def initializeOrbitals(self, nOrbitals, nElectrons, nOrbitalSteps, electronSize=5):
+		phi = (math.sqrt(5)+1)/2
+		ellipticalRange = [1, phi*phi]
+		radiusRange = [1/phi, phi]
+		speedRange = [1/phi, phi]
+
+		allOrbitals = []
+
+		for orbital in range(nOrbitals):
+			
+			orbitProperties = {'points': [], 'electron locations': [], 'color': '#036BFC', 'point size': electronSize}
+
+			# angles and dynamic radius
+			angleRadList = self.dynamicRadius(radiusRange, ellipticalRange, speedRange, nOrbitalSteps)
+			orbitProperties['angle & radius list'] = angleRadList
+			orbitProperties['rotation speeds'] = [random.uniform(-0.2, 0.2) for i in range(3)]
+
+			# transformation into 3D space
+			orbitProperties = self.transformOrbital(orbitProperties)
+
+			allOrbitals.append(orbitProperties)
+		
+		orbitalSet = self.distributeElectrons(allOrbitals, nElectrons)
+
+		self.orbitalSet = orbitalSet
+
+
+	def dynamicRadius(self, radiusRange, ellipticalRange, speedRange, nOrbitalSteps):
+		axisA = random.uniform(radiusRange[0], radiusRange[1])
+		axisB = axisA * random.uniform(ellipticalRange[0], ellipticalRange[1])
+		orbitalSpeed = random.uniform(speedRange[0], speedRange[1])
+		nSteps = round(nOrbitalSteps/orbitalSpeed)
+		angleOffset = random.uniform(0, 2*math.pi)
+		angleList = [(stepN*2*math.pi)/nSteps for stepN in range(nSteps)]
+		radiusList = [self.calcRadius(axisA, axisB, angleList[i] + angleOffset) for i in range(nSteps)]
+
+		angleRadList = [{'angle': angleList[i], 'radius': radiusList[i]} for i in range(nSteps)]
+
+		return angleRadList
+
+
+	def calcRadius(self, a, b, t):
+		return a*b/math.sqrt(pow(a*math.sin(t), 2)+pow(b*math.cos(t), 2))
+
+
+	def transformOrbital(self, orbitProperties):
+		angleRadList = orbitProperties['angle & radius list']
+		nSteps = len(angleRadList)
+
+		thetas = [random.uniform(0, 2*math.pi) for i in range(3)]
+		multMatrix = matricesData.regRotationMat(thetas)
+		
+		cartesianList = polarToCartesian(angleRadList)
+		pointsMatrix = findTranspose(cartesianList)
+		projectedPoints = np.dot(multMatrix, pointsMatrix)
+		points = findTranspose(projectedPoints)
+		
+		orbitProperties['points'] = points
+
+		return orbitProperties
+
+
+	def distributeElectrons(self, orbitalSet, nElectrons):
+		nOrbitals = len(orbitalSet)
+
+		for electronN in range(nElectrons):
+			orbitIndex = random.randint(0, nOrbitals - 1)
+			nSteps = len(orbitalSet[orbitIndex]['points'])
+			startingAngle = random.randint(0, nSteps - 1)
+
+			orbitalSet[orbitIndex]['electron locations'].append(startingAngle)
+		
+		return orbitalSet
+
+
 	def updateElectrons(self):
 		for orbitalN in range(len(self.orbitalSet)):
 			orbital = self.orbitalSet[orbitalN]
@@ -149,8 +121,17 @@ class AtomSchematic(Schematic):
 
 			nSteps = len(orbital['points'])
 			electrons = orbital['electron locations']
-			electrons = passElectrons(electrons, nSteps)
+			electrons = self.passElectrons(electrons, nSteps)
 			self.orbitalSet[orbitalN]['electron locations'] = electrons
+
+
+	def passElectrons(self, electrons, nSteps):
+		for e in range(len(electrons)):
+			if electrons[e] in [nSteps - 1, nSteps]:
+				electrons[e] = 0
+			else:
+				electrons[e] += 1
+		return electrons
 
 
 def polarToCartesian(angleRadList):
