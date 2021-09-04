4fbde2aa01955b7cbc83bb048db0e3460479d156
Rainethhh <Rainethcubed@gmail.com>
2021-09-04 13:57:17 -0500

    dynamic schematics editable
    It is now possible to remove features from a schematic with ease

--- rendererWorkStation/schematicLab.py
+++ rendererWorkStation/schematicLab.py
@@ -26,60 +26,73 @@ def schematicLabScript():
 
 
 class schematicLabData(DataHolder):
+
+
+	# Customize schematics
+
+	def customizeAtom(self):
+		visible = True
+		schemName = 'cool atom'
+		schemObject = AtomSchematic(nOrbitals=10, nElectrons=100, nOrbitalSteps=100, electronSize=3)
+		includedFeatures = {'points': True, 'lines': True}
+
+		if visible: self.addToNamedSchems(schemName, schemObject, includedFeatures)
+
+
+	def customizeCube(self):
+		visible = True
+		schemName = 'cool cube'
+		schemObject = CubeSchematic(cubeRadius=0.05, color='#50E060', pointSize=2)
+		includedFeatures = {'points': False, 'lines': True, 'triangles': False}
+
+		if visible: self.addToNamedSchems(schemName, schemObject, includedFeatures)
+
+	
+	def customizeRatio(self):
+		visible = True
+		schemName = 'cool ratio'
+		schemObject = RatioSchem(trials=999, color='#00FF00')
+		includedFeatures = {'points': True}
+
+		if visible: self.addToNamedSchems(schemName, schemObject, includedFeatures)
+
+	# Customization ends here
+
+
 	def __init__(self):
 		super().__init__()
 		self.namedSchematics = {}
-		
+
 		self.activateSchematics()
 
 
-	# Which schematics are being used? 
-
 	def activateSchematics(self):
-		self.useAtomSchematic()
-		self.useCubeSchematic()
-		#self.useRatioSchematic()
+		self.customizeAtom()
+		self.customizeCube()
+		self.customizeRatio()
 
-
-	# Customize Schematics
-
-	def useAtomSchematic(self):
-		self.coolAtom = AtomSchematic(nOrbitals=10, nElectrons=100, nOrbitalSteps=100, electronSize=3)
-
-		self.addToNamedSchems(self.coolAtom, 'cool atom')
-
-
-	def useCubeSchematic(self):
-		self.coolCube = CubeSchematic(cubeRadius=0.05, color='#50E060', pointSize=2)
-		self.coolCube.removeElements('points', 'triangles')
-
-		self.addToNamedSchems(self.coolCube, 'cool cube')
-
-
-	def useRatioSchematic(self):
-		self.coolRatio = RatioSchem(trials=999)
-
-		self.addToNamedSchems(self.coolRatio, 'cool ratio')
-
-
-	# Customize schematic descendants
-
-	# ...
-
-	# Customization ends here
 
 	def updateSchematics(self):
 		for schemName in self.namedSchematics:
 			if self.namedSchematics[schemName]['is dynamic']:
 				self.namedSchematics[schemName]['schem object'].updateSchematic()
 
+		self.removeDiscludedFeatures()
 		self.schematic = combineSchematics(addedSchematics=[schematicStructure.namedSchematics[schemName]['schem object'].schematic for schemName in schematicStructure.namedSchematics])
 
 
-	def addToNamedSchems(self, schemObject, schemName):
+	def addToNamedSchems(self, schemName, schemObject, includedFeatures):
 		self.namedSchematics[schemName] = {}
 		self.namedSchematics[schemName]['schem object'] = schemObject
 		self.namedSchematics[schemName]['is dynamic'] = hasParent(schemObject, 'DynamicSchematic')
+		self.namedSchematics[schemName]['included features'] = includedFeatures
+
+
+	def removeDiscludedFeatures(self):
+		for schemName in self.namedSchematics:
+			for featureType in self.namedSchematics[schemName]['included features']:
+				if not self.namedSchematics[schemName]['included features'][featureType]:
+					self.namedSchematics[schemName]['schem object'].removeElements(featureType)
 
 
 
